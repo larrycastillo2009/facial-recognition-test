b17547e8008d9833d9cc075ea5efcdee3a5d90d1
Larry Castillo <larrycastillo2009@gmail.com>
2021-06-21 01:58:41 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const video = document.getElementById('video')
+
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri('')
+])
+
+function startVideo(){
+  navigator.getUserMedia(
+    { video: {} },
+    stream => video.srcObject = stream,
+    err => console.error(err)
+  )
+}
+startVideo();
